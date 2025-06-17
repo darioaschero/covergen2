c4319f2c87530d3554a48aaab6f70030b9e07453
Dario Aschero <dario.aschero@gmail.com>
2025-06-17 13:04:20 +0200

    vite tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center overflow-hidden">
+      <div className="w-96 h-96 bg-red-500 flex items-center justify-center">
+        <h1 className="text-white text-2xl">Testing Tailwind</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
